b017369fefc9eb34cf1974338abd7e66e9d59b99
Andzhey Koba <andzheykoba@gmail.com>
2016-06-23 17:12:08 +0300

    My-HW02-layers

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -6,12 +6,9 @@ import java.time.LocalDateTime;
  * GKislin
  * 11.01.2015.
  */
-public class UserMeal {
-    private Integer id;
+public class UserMeal extends BaseEntity {
     private final LocalDateTime dateTime;
-
     private final String description;
-
     private final int calories;
 
     public UserMeal(LocalDateTime dateTime, String description, int calories) {
@@ -19,14 +16,10 @@ public class UserMeal {
     }
 
     public UserMeal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
@@ -41,18 +34,10 @@ public class UserMeal {
         return calories;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public boolean isNew() {
-        return id == null;
-    }
-
     @Override
     public String toString() {
         return "UserMeal{" +
-                "id=" + id +
+                "id" + id +
                 ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
